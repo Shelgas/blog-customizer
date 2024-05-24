1da5e0127a20ad231cf3853e78d2ae54047a1d6b
Shelgas <akozlov1997@gmail.com>
2024-05-24 18:07:07 +0300

    removed lint error

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,32 @@
 import arrow from 'src/images/arrow.svg';
-
+import clsx from 'clsx';
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type StateProps = {
+	isOpen: boolean;
+	onClick: () => void;
+};
+
+export const ArrowButton = (props: StateProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(
+				styles.container,
+				props.isOpen ? styles.container_open : ''
+			)}
+			onClick={props.onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, props.isOpen && styles.arrow_open)}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,19 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const arrowButtonHalndler = () => setIsMenuOpen(!isMenuOpen);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isMenuOpen} onClick={arrowButtonHalndler} />
+			<aside
+				className={`${styles.container} 
+				${isMenuOpen && styles.container_open}`}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
